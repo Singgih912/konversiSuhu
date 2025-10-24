a36e1078850af251b2a78f6912b530094fd2cfb7
Restu Singgih <resthusgh09@gmail.com>
2025-10-24 23:38:36 +0700

    Firts commit - Konversi Suhu Project

---
+++ konversiSuhu.js
@@ -0,0 +1,10 @@
+const chalk = require('chalk'); // package eksternal
+const { celciusToFahrenheit, } = require('./rumusSuhu');
+
+const suhuCelcius = 30;
+const suhuFahrenheit = celciusToFahrenheit(suhuCelcius);
+
+console.log(chalk.blue.bold('===Program Konversi Suhu==='));
+console.log(chalk.green(`Suhu dalam Celcius; ${suhuCelcius}°C`));
+console.log(chalk.yellow(`Hasil konversi ke Fahrenheit: ${suhuFahrenheit}°F`));
+console.log(chalk.magenta('Terima kasih telah menggunakan program ini!')); 
---
+++ rumusSuhu.js
@@ -0,0 +1,6 @@
+function celciusToFahrenheit(c) {
+    return (c * 9/5) + 32;
+
+}
+
+module.exports = { celciusToFahrenheit };
