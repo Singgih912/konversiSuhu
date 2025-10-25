3ebd94cfe88330079be125fae1a7da26a81a39ee
Restu Singgih <resthusgh09@gmail.com>
2025-10-25 20:11:05 +0700

    Tugas 5 - Pemrograman Berbasis Platform

--- konversiSuhu.js
+++ konversiSuhu.js
@@ -5,6 +5,7 @@ const suhuCelcius = 30;
 const suhuFahrenheit = celciusToFahrenheit(suhuCelcius);
 
 console.log(chalk.blue.bold('===Program Konversi Suhu==='));
+console.log(chalk.red.underline('Nama: Restu Singgih & Diffa Syafiq Juhaeni'));
 console.log(chalk.green(`Suhu dalam Celcius; ${suhuCelcius}°C`));
 console.log(chalk.yellow(`Hasil konversi ke Fahrenheit: ${suhuFahrenheit}°F`));
 console.log(chalk.magenta('Terima kasih telah menggunakan program ini!')); 
